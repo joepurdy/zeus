331ce88c8e9c3c4c907c65eab9c9dfd4bc399bb0
Joe Purdy <joe@purdy.dev>
2022-07-17 03:46:58 +0000

    fix(gitpod): move aws env config to script

---
+++ .gitpod/scripts/awsenv.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+gp env -u AWS_ACCESS_KEY_ID
+gp env -u AWS_SECRET_ACCESS_KEY
+
+read -p "Enter AWS Access Key: " access_key
+read -p "Enter AWS Secret Key: " secret_key
+
+gp env AWS_ACCESS_KEY_ID=$access_key
+gp env AWS_SECRET_ACCESS_KEY=$secret_key
+gp env AWS_REGION=us-west-2
+
+echo "eval \$(gp env -e)" >>~/.bashrc
