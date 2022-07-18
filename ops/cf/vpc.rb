31c928a265cdd952a6496b0f43a3d5e09fd27421
Joe Purdy <joe@purdy.dev>
2022-07-18 06:19:55 +0000

    refactor(vpc): remove vpc stack

--- ops/cf/vpc.rb
+++
@@ -1,7 +0,0 @@
-description 'vpc stack'
-
-include_template(
-  'vpc/vpc.rb',
-  'vpc/subnets.rb',
-  'vpc/endpoints.rb'
-)
--- ops/lib/stack/db.rb
+++ ops/lib/stack/db.rb
@@ -1,17 +1,17 @@
 module Stax
   class Db < Stack
-    no_commands do
-      def cfer_parameters
-        {
-          vpc: stack(:vpc).stack_name
-        }
-      end
-    end
+    # no_commands do
+    #   def cfer_parameters
+    #     {
+    #       vpc: stack(:vpc).stack_name
+    #     }
+    #   end
+    # end
 
-    desc 'create', 'create stack'
-    def create
-      ensure_stack :vpc   # make sure vpc stack is created first
-      super               # create the stack
-    end
+    # desc 'create', 'create stack'
+    # def create
+    #   ensure_stack :vpc   # make sure vpc stack is created first
+    #   super               # create the stack
+    # end
   end
 end
