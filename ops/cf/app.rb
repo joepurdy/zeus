5dcd46d54eff135f41aeb1f82f2accb86300d62f
Joe Purdy <joe@purdy.dev>
2022-07-18 00:44:07 +0000

    chore(app): comment out unused parameters

--- ops/cf/app.rb
+++ ops/cf/app.rb
@@ -1,8 +1,11 @@
 description 'app stack'
 
+# This config wasn't used because it relies on a custom domain and SSL certificate
+# See note in "Going further" section of README about removing custom domain due to lack of SSL certificate automation w/ ACM
+#
 ## DNS domains for route53
-parameter :domain, type: :String, default: 'hjkl.cloud'
-parameter :subdomain, type: :String, default: 'zeus'
+# parameter :domain, type: :String, default: 'hjkl.cloud'
+# parameter :subdomain, type: :String, default: 'zeus'
 
 include_template(
   'app/cloudfront.rb',
