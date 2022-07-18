d954aae45fa97f6b92c21696eb144e70de8cd163
Joe Purdy <joe@purdy.dev>
2022-07-18 06:00:28 +0000

    feat(gitpod): automatically provision resources in dev

---
+++ .gitpod/scripts/awscli.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+aws --version
+
+rm -rf aws/ awscliv2.zip
--- .gitpod/scripts/awsenv.sh
+++ .gitpod/scripts/awsenv.sh
@@ -2,12 +2,15 @@
 
 gp env -u AWS_ACCESS_KEY_ID
 gp env -u AWS_SECRET_ACCESS_KEY
+gp env -u AWS_ACCOUNT_ID
 
 read -p "Enter AWS Access Key: " access_key
 read -p "Enter AWS Secret Key: " secret_key
+read -p "Enter AWS Account Number: " account_id
 
 gp env AWS_ACCESS_KEY_ID=$access_key
 gp env AWS_SECRET_ACCESS_KEY=$secret_key
+gp env AWS_ACCOUNT_ID=$account_id
 gp env AWS_REGION=us-east-1
 
 echo "eval \$(gp env -e)" >>~/.bashrc
